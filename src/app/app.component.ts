84d5b2f356d5482b8a23bb845cc90bba4d6e8b41
Samuel Adebayo <sambayo28@gmail.com>
2022-03-25 11:12:58 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'beyonic-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'beyonic';
+}
